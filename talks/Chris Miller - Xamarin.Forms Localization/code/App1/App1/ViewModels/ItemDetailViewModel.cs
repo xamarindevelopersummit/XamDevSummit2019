bad06d9ed2d4b74070f8856881e028269b6bd5d3
Miller, Chris <chris.miller@Chris-Millers-MacBook-Pro-2.local>
2019-07-16 14:49:15 -0400

    Added files for Localization session

---
+++ talks/Chris Miller - Xamarin.Forms Localization/code/App1/App1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using App1.Models;
+
+namespace App1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
